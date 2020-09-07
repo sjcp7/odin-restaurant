5a811c90db7aa3d6c1177920133f7c4223d6529a
Samuel Pedro <redsamuel.cruz7@gmail.com>
2020-09-07 21:05:15 +0100

    Change some settings

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,38 @@
-const path = require('path');
+  const path = require('path');
 
-module.exports = {
-  entry: './src/index.js',
-  output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
-};
+  module.exports = {
+    entry: './src/scripts/index.js',
+    output: {
+      filename: 'main.js',
+      path: path.resolve(__dirname, 'dist'),
+    },
+    module: {
+      rules: [
+        {
+          test: /\.css$/,
+          use: [
+            {
+              loader: 'style-loader'
+            },
+            {
+              loader: 'css-loader',
+              options: {
+                sourceMap: true
+              }
+            }
+          ],
+        },
+       {
+         test: /\.(png|svg|jpg|gif)$/,
+         use: [
+           {
+             loader: 'file-loader',
+             options: {
+              outputPath: 'images'
+             }
+           }
+         ],
+       },
+      ],
+    },
+  };
